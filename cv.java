a78bf70c7218ecdf9bc6c91c1eb459e5803acee9
Adam <adambaho@outlook.com>
2020-02-03 11:20:06 -0500

    test

---
+++ cv.java
@@ -0,0 +1,24 @@
+public class cv {
+	
+	public static void main() {
+		System.out.println("Bienvenue chez Barette! \n");
+		
+		affiche();
+	}
+	
+	public static String affiche(){
+		String nom = "Baho ", 
+				prenom = "Adam ", 
+				Formation = "Informaticien ", 
+				Experience = "Aucune ", 
+				Competences = "Diverses ", 
+				Attentes = "Passé le cours à 100% ";
+				
+			
+		String cv = "\n" + nom + prenom + Formation + Experience + Competences + Attentes;
+		
+		return cv;
+	}
+	
+}
+
